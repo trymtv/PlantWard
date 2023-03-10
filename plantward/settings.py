e81393e0fd244f531db3cd1909c07c6666d7f9ac
epdoan <epdoan@stud.ntnu.no>
2023-03-10 14:21:22 +0000

    removed pycache tracking

--- plantward/settings.py
+++ plantward/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-h0ek1-ehhra#l^iatlswn_(z&)+y8jm^$6exgz@0&l%=0izn34
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
