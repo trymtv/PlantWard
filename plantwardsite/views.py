0bb38ec0cff2b368a8c5dd27f4b595bdeb8e17b6
epdoan <epdoan@stud.ntnu.no>
2023-03-24 08:23:37 +0000

    made dummy plants and field to change threshold

--- plantwardsite/views.py
+++ plantwardsite/views.py
@@ -1,25 +1,41 @@
 import json
 import random
 from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse, JsonResponse, HttpResponseRedirect
 from django.views.decorators.csrf import csrf_exempt
 from plantwardsite.models import Measurement, SensorDetails
 from plantwardsite.mqtt import client
 from django.forms.models import model_to_dict
 
+def create_dummies():
+    last_measurement = Measurement.objects.last()
+    dummies = [last_measurement]
+    for i in range(5):
+        dummy = Measurement()
+        dummy.soil_hum = random.randrange(20, 50)
+        dummy.air_hum = random.randrange(20, 50)
+        dummy.air_temp = random.randrange(10, 30)
+        dummies.append(dummy)
+    return dummies
+
+
 def index(request):
     last_measurement = Measurement.objects.last()
     threshold = SensorDetails.objects.last()
     rand_num = random.randrange(-2, 3)
+    dummies = create_dummies()
     return render(request, 'index.html', {"sensor_values": last_measurement, "threshold":threshold, 
-    "range": range(1,4), "rand_num": rand_num })
+    "range": range(1,4), "rand_num": rand_num, "dummies": dummies})
 
 @csrf_exempt
 def threshold(request):
     if request.method == "POST":
-        body = json.loads(request.body)
-        rc, mid = client.publish("sensor/threshold", body['threshold'])
-        return JsonResponse({"responseCode": rc})
+        threshold = request.POST["threshold"]
+        rc, mid = client.publish("sensor/threshold", threshold)
+        details = SensorDetails.objects.last()
+        details.threshold = threshold
+        details.save()
+        return HttpResponseRedirect("/")
     return HttpResponse(status=405)
 
 @csrf_exempt
